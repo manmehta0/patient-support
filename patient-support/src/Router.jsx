693161ef8b1b2def113dd9842fae4ec5fdcd202e
Manoj Mehta <meh.manoj@gmail.com>
2024-09-16 09:21:26 -0700

    update

--- patient-support/src/Router.jsx
+++ patient-support/src/Router.jsx
@@ -5,17 +5,15 @@ import Logo from './assets/images/logo.png';
 
 export const AppRouter = ({ patientId }) => {
   return (
-    <>
-      <Router>
-        <Switch>
-          <Route exact path='/'>
-            <Redirect to='/dashboard' />
-          </Route>
-          <Route exact path='/dashboard'>
-            <Dashboard patientId={patientId} />
-          </Route>
-        </Switch>
-      </Router>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Redirect to='/dashboard' />
+        </Route>
+        <Route exact path='/dashboard'>
+          <Dashboard patientId={patientId} />
+        </Route>
+      </Switch>
       <div className='min-h-screen bg-gray-100'>
         <nav className='bg-blue-600 text-white p-4'>
           <div className='container mx-auto'>
@@ -26,6 +24,6 @@ export const AppRouter = ({ patientId }) => {
           </div>
         </nav>
       </div>
-    </>
+    </Router>
   );
 };
